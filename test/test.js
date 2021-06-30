c02485c409eba1f23acb0a3f883a877fe7b4b8c1
adhywiranto44 <adhywiranto68@gmail.com>
2021-06-30 18:30:47 +0700

    Adding testing for Hosted server (twice-api.adhywiranto.xyz)

--- test/test.js
+++ test/test.js
@@ -1,7 +1,7 @@
 const assert = require('assert');
 const http = require('http');
 
-describe("Testing TWICE API", () => {
+describe("Testing TWICE API Local", () => {
 	before(() => {
 		console.log("Starting . . .");
 	});
@@ -16,42 +16,100 @@ describe("Testing TWICE API", () => {
 		});
 
 		it("GET /api/all", (done) => {
-			http.get('http://localhost:3000/api/all', (response) => {
+			http.get('http://localhost:4000/api/all', (response) => {
 				assert.strictEqual(response.statusCode, 200);
 				done();
 			});
 		});
 
 		it("GET /api/members", (done) => {
-			http.get('http://localhost:3000/api/members', (response) => {
+			http.get('http://localhost:4000/api/members', (response) => {
 				assert.strictEqual(response.statusCode, 200);
 				done();
 			});
 		});
 
 		it("GET /api/discography", (done) => {
-			http.get('http://localhost:3000/api/discography', (response) => {
+			http.get('http://localhost:4000/api/discography', (response) => {
 				assert.strictEqual(response.statusCode, 200);
 				done();
 			});
 		});
 
 		it("GET /api/discography/korean", (done) => {
-			http.get('http://localhost:3000/api/discography/korean', (response) => {
+			http.get('http://localhost:4000/api/discography/korean', (response) => {
 				assert.strictEqual(response.statusCode, 200);
 				done();
 			});
 		});
 
 		it("GET /api/discography/japanese", (done) => {
-			http.get('http://localhost:3000/api/discography/japanese', (response) => {
+			http.get('http://localhost:4000/api/discography/japanese', (response) => {
 				assert.strictEqual(response.statusCode, 200);
 				done();
 			});
 		});
 
 		it("GET /api/discography/english", (done) => {
-			http.get('http://localhost:3000/api/discography/english', (response) => {
+			http.get('http://localhost:4000/api/discography/english', (response) => {
+				assert.strictEqual(response.statusCode, 200);
+				done();
+			});
+		});
+	});
+});
+
+describe("Testing TWICE API Hosted (twice-api.adhywiranto.xyz)", () => {
+	before(() => {
+		console.log("Starting . . .");
+	});
+
+	after(() => {
+		console.log("Done testing all routes!");
+	});
+
+	context("Get Request", () => {
+		beforeEach(() => {
+			console.log("Executing each requests.");
+		});
+
+		it("GET /api/all", (done) => {
+			http.get('http://twice-api.adhywiranto.xyz/api/all', (response) => {
+				assert.strictEqual(response.statusCode, 200);
+				done();
+			});
+		});
+
+		it("GET /api/members", (done) => {
+			http.get('http://twice-api.adhywiranto.xyz/api/members', (response) => {
+				assert.strictEqual(response.statusCode, 200);
+				done();
+			});
+		});
+
+		it("GET /api/discography", (done) => {
+			http.get('http://twice-api.adhywiranto.xyz/api/discography', (response) => {
+				assert.strictEqual(response.statusCode, 200);
+				done();
+			});
+		});
+
+		it("GET /api/discography/korean", (done) => {
+			http.get('http://twice-api.adhywiranto.xyz/api/discography/korean', (response) => {
+				assert.strictEqual(response.statusCode, 200);
+				done();
+			});
+		});
+
+		it("GET /api/discography/japanese", (done) => {
+			http.get('http://twice-api.adhywiranto.xyz/api/discography/japanese', (response) => {
+				assert.strictEqual(response.statusCode, 200);
+				done();
+			});
+		});
+
+		it("GET /api/discography/english", (done) => {
+			http.get('http://twice-api.adhywiranto.xyz/api/discography/english', (response) => {
 				assert.strictEqual(response.statusCode, 200);
 				done();
 			});
